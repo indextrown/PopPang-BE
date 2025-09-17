767a13b4c99a6678145a052b8474bc58e13e3e50
indextrown <indextrown@gmail.com>
2025-09-17 21:33:26 +0900

    feat: UserRepository 구현

---
+++ src/main/java/com/poppang/api/poppangtest/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.poppang.api.poppangtest.repository;
+
+import com.poppang.api.poppangtest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JpaRepository는 JAP에서 제공하는 기본 인터페이스이다 <엔티티, 엔티티의pk>를 받는다
+// DB(User 테이블)와 연결되는 인터페이스
+// 직접 SQL을 작성하지 않아도 기본 CRUD 메서드를 제공한다
+public interface UserRepository extends JpaRepository<User, String> {
+    // JpaRepository<User, String> 이므로 findById() 메서드를 바로 사용 가능
+    // findById(String id)
+    // findAll()
+    // save(User user)
+    // delete(User user)
+    // count()
+}
